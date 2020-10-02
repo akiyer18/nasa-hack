2445c09cc7fcbca4ae13d2cebfbf91c21b547331
coolsm12 <shivam.mahajan0912@gmail.com>
2020-10-02 16:33:44 +0530

    reduced inputs

--- gui/web/main.js
+++ gui/web/main.js
@@ -1,15 +1,13 @@
 function calljs() {
-	var data1 = document.getElementById("data1").value
-	var data2 = document.getElementById("data2").value
-	var data3 = document.getElementById("data3").value
-	var data6 = document.getElementById("data6").value
-	var data7 = document.getElementById("data7").value
-	var data8 = document.getElementById("data8").value
+	
 	var e = document.getElementById("work");
-	var strUser = e.options[e.selectedIndex].value;
-	var f = document.getElementById("medication");
-    var strUser2 = f.options[f.selectedIndex].value;
-	var data_array=[data1,data2,data3,data6,data7,data8,strUser,strUser2]
+	var work_numeric = e.options[e.selectedIndex].value;
+	var data_workhours = document.getElementById("data_workhours").value
+	var data_diet = document.getElementById("data_diet").value
+	var data_noise = document.getElementById("data_noise").value
+    var f = document.getElementById("medication");
+    var medication_numeric = f.options[f.selectedIndex].value;
+	var data_array=[work_numeric, data_workhours, data_diet, data_noise, medication_numeric]
     eel.pass_to_python(data_array)
 
 }
