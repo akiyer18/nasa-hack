d950deb533c060033198caa63b27c2859d9992e7
coolsm12 <shivam.mahajan0912@gmail.com>
2020-10-02 16:50:50 +0530

    new page added

--- gui/web/main.js
+++ gui/web/main.js
@@ -8,7 +8,8 @@ function calljs() {
     var f = document.getElementById("medication");
     var medication_numeric = f.options[f.selectedIndex].value;
 	var data_array=[work_numeric, data_workhours, data_diet, data_noise, medication_numeric]
-    eel.pass_to_python(data_array)
+	eel.pass_to_python(data_array)
+	window.location = 'page3.html';
 
 }
 
